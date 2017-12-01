f772cfff0eb1d95018ed31693dd2b09805ccaf30
Dan Jaggs <djaggs@gmail.com>
2017-12-01 17:37:03 +0000

    v0.1

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,10 +2,11 @@
 using System.Collections;
 
 public class Ball : MonoBehaviour {
-	private Paddle paddle;
 	
+	private Paddle paddle;
 	private bool hasStarted = false;
 	private Vector3 paddleToBallVector;
+	
 	// Use this for initialization
 	void Start () {
 	paddle = GameObject.FindObjectOfType<Paddle>();
@@ -24,7 +25,16 @@ public class Ball : MonoBehaviour {
 			print ("Mouse clicked, launch ball");
 			hasStarted = true;
 			this.rigidbody2D.velocity = new Vector2 (2f, 10f);
-		}	
-		}
+	 }	
+   }
+ }
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		Vector2 tweak = new Vector2 (Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+		
+		if (hasStarted) {
+			audio.Play ();
+			rigidbody2D.velocity += tweak;
 	}
+  }
 }
